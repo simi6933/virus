540816a3573ed2e2c72784dc126b2fcd6a87724f
simi6933 <64491440+simi6933@users.noreply.github.com>
2020-04-28 19:38:26 +0200

    Create virus.js

---
+++ virus.js
@@ -0,0 +1,23 @@
+var sim = (function () {
+
+  function init() {
+  }
+  
+  function start() {
+  }
+  
+  function nextDay() {
+  }
+  
+  function getResults() {
+  }
+  
+  return {
+    init: init,
+    start: start,
+    nextDay: nextDay,
+    getResults: getResults,
+  };
+})();
+
+sim.init();
