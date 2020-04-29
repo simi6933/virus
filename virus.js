83dc64df52ec8ae8e2257f3f4cd2757152d16a58
dzsumika <zsombor.simonguth@gmail.com>
2020-04-29 20:12:57 +0200

    Simple Simulation

--- virus.js
+++ virus.js
@@ -1,15 +1,67 @@
 var sim = (function () {
 
   function init() {
+    r0 = parseInt(document.getElementById("r0").value);
+    population = parseInt(document.getElementById("population").value);
+    incubationPeriod = parseInt(document.getElementById("incubationPeriod").value);
+    illnessPeriod = parseInt(document.getElementById("illnessPeriod").value);
+    daysToCalculate = parseInt(document.getElementById("daysToCalculate").value);
+
+    areHealthy = [];
+    inIncubation = [];
+    areIll = [];
+    areImmune = [];
+
+    healthy = (population - 1); // egyenlöre hardcodeoltam az egy megbetegedettet
+    incubation = 1;
+    ill = 0;
+    immune = 0;
+    currentDay = 0;
+    contractedPerDay = 0;
+
+    start();
   }
   
   function start() {
+
+    while(!(currentDay == daysToCalculate))
+    {
+      console.log("Current Day: " + currentDay + " | Days to Calculate: " + daysToCalculate);
+      nextDay();
+      currentDay++;
+    }
+    getResults();
   }
   
   function nextDay() {
+
+    contractedPerDay += (incubation + ill) * (r0 / (incubationPeriod + illnessPeriod));
+    console.log(incubationPeriod + illnessPeriod);
+    if(currentDay >= illnessPeriod) immune += areIll[currentDay - illnessPeriod];
+    if(currentDay >= incubationPeriod) ill += inIncubation[currentDay - incubationPeriod];
+
+    (healthy != 0) ? (incubation += (healthy - (healthy - contractedPerDay))) : (ill += incubation, incubation = 0);
+    ((healthy - contractedPerDay) <= 0) ? (incubation += healthy, healthy = 0) : (healthy -= contractedPerDay);
+
+    areHealthy[currentDay] = Math.round(healthy);
+    inIncubation[currentDay] = Math.round(incubation);
+    areIll[currentDay] = Math.round(ill);
+    areImmune[currentDay] = Math.round(immune);
   }
   
   function getResults() {
+
+    console.log("Healthy People: ", areHealthy);
+    console.log("People in incubations: ", inIncubation);
+    console.log("Ill people: ", areIll);
+    console.log("Immune people: ", areImmune);
+
+    return {
+      areHealthy: areHealthy,
+      inIncubation: inIncubation,
+      areIll: areIll,
+      areImmune: areImmune,
+    };
   }
   
   return {
@@ -22,8 +74,12 @@ var sim = (function () {
 
 var unittest = (function () {
   var tests = {
-    dummyTrueTest: function () {
-      return true;
+    initTest: function () {
+      
+    },
+
+    getResultsTest: function () {
+      console.log(sim.areHealthy);
     },
 
     dummyFalseTest: function () {
@@ -45,4 +101,3 @@ var unittest = (function () {
 })();
 
 unittest.run();
-sim.init();
